831de9abf20ca0a02f160fdd9b6c321d55698d43
mehdi <mehdigassabarsa@gmail.com>
2024-03-17 22:01:20 +0100

    Core(SP1hu21): Added leaflet, tailwind and router. And showed map on home view

---
+++ src/components/Map.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import L, {geoJSON, latLng, LeafletMouseEvent, Map} from "leaflet";
+import "leaflet/dist/leaflet.css";
+import {onMounted, ref} from 'vue';
+
+const props = defineProps<{
+  initLatLang: L.LatLngExpression,
+  zoom: number
+}>();
+
+const map = ref<Map>();
+
+
+onMounted(() => {
+   map.value = L.map("map", {
+    center: props.initLatLang,
+    zoom: props.zoom
+  });
+
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+        'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+    maxZoom: 18,
+  }).addTo(map.value);
+
+});
+
+</script>
+
+<template>
+  <div id="map" style="height: 80vh; width: 100%;"></div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Map from "../components/Map.vue";
+import {ref} from "vue";
+
+const initLatLang: L.LatLngExpression = [39.98541896850344, -0.05080976072749943];
+const initZoom: number = 13;
+const map = ref();
+</script>
+
+<template>
+  <div></div>
+  <div class="flex md:flex-row sm:flex-col">
+    <Map class="rounded-lg" :init-lat-lang="initLatLang" :zoom="initZoom" ref="map"></Map>
+  </div>
+</template>
