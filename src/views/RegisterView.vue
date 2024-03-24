59477e2738c275b457484ff040e85e708ba42e89
eric <erickmontes555@gmail.com>
2024-03-24 11:52:18 +0100

    Add register view and form component

---
+++ src/views/RegisterView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import RegisterFormComponent from '../components/RegisterFormComponent.vue'
+</script>
+
+<template>
+    <div class="h-full flex flex-col align-center ">
+        <div class="card w-2/4 items-center m-auto">
+            <div class="card-body prose">
+                <h2 class="card-title">Register!</h2>
+                <RegisterFormComponent />
+            </div>
+        </div>
+    </div>
+</template>
