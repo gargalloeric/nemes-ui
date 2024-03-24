47ddfc3c558c298f151a2cde6a400c0432db7bec
eric <erickmontes555@gmail.com>
2024-03-24 11:51:34 +0100

    Remove example view and component

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from './views/HomeView.vue'
+import RegisterView from './views/RegisterView.vue'
 
 const routes = [
     {
-        path: '/',
-        component: HomeView
+        path: '/register',
+        component: RegisterView
     }
 ]
 
