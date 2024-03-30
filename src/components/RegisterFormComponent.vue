1fe872fd2fa60abb7e6821e5a616d81756de267d
eric <erickmontes555@gmail.com>
2024-03-30 13:29:48 +0100

    Add initial user feedback when register

--- src/components/RegisterFormComponent.vue
+++ src/components/RegisterFormComponent.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
+import { Ref, ref } from 'vue';
 import { RegisterQuery } from '../models/RegisterQuery';
 
-const emit = defineEmits(['form-query'])
+const emit = defineEmits(['form-query', 'show-error'])
 
+const passError: Ref<boolean> = ref(false)
 let form: RegisterQuery = {
     username: '',
     email: '',
@@ -12,10 +14,14 @@ let form: RegisterQuery = {
 }
 
 function handleSubmit() {
-    if (form.password != form.repassword)
-        // TODO: Handle this case the right way.
-        return
+    if (form.password != form.repassword) {
+        // TODO: Improve error handling and feedback.
+        emit('show-error', 'Las contraseñas no coinciden.');
+        passError.value = true;
+        return;
+    }
     emit('form-query', form)
+    passError.value = false;
 }
 </script>
 
@@ -49,7 +55,7 @@ function handleSubmit() {
             <span class="badge badge-info">Opcional</span>
         </label>
 
-        <label class="input input-bordered flex items-center gap-2 mb-2">
+        <label class="input input-bordered flex items-center gap-2 mb-2" :class="{ 'input-error': passError }">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                 stroke="currentColor" class="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round"
@@ -58,11 +64,17 @@ function handleSubmit() {
             <input type="password" placeholder="Contraseña" v-model.trim="form.password" class="grow" required />
         </label>
 
-        <label class="input input-bordered flex items-center gap-2 mb-2">
+
+        <label class="input input-bordered flex items-center gap-2 mb-2" :class="{ 'input-error': passError }">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
+            </svg>
             <input type="password" placeholder="Repite la contraseña" v-model.trim="form.repassword" class="grow"
                 required />
         </label>
 
-        <button class="btn btn-primary btn-block mt-5">Register</button>
+        <button class="btn btn-primary btn-block mt-5">Registrar</button>
     </form>
 </template>
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,7 +1,14 @@
 <script setup lang="ts">
+import { Ref, ref } from 'vue';
 import RegisterFormComponent from '../components/RegisterFormComponent.vue'
+import SuccessAlertComponent from '../components/SuccessAlertComponent.vue'
+import ErrorAlertComponent from '../components/ErrorAlertComponent.vue'
 import { RegisterQuery } from '../models/RegisterQuery';
 import { BASE_URL } from '../utils/Constants';
+
+const showAlertSuccess: Ref<boolean> = ref(false);
+const showAlertError: Ref<boolean> = ref(false);
+let errorMsg: string = '';
 
 async function handleFormQuery(form: RegisterQuery) {
     const REGISTER_URL: string = `${BASE_URL}/nuser/create`
@@ -19,20 +26,38 @@ async function handleFormQuery(form: RegisterQuery) {
     })
 
     // TODO: Handle user created and user not created
-    if (!resp.ok) {
-        console.log('User not created')
+    if (resp.ok) {
+        showAlertSuccess.value = true;
     } else {
-        console.log('User created')
+        console.log('User not created')
     }
+}
+
+function handleClose() {
+    showAlertSuccess.value = false;
+    showAlertError.value = false;
+}
+
+function handleShowError(msg: string) {
+    errorMsg = msg;
+    showAlertError.value = true;
 }
 </script>
 
 <template>
+    <SuccessAlertComponent v-if="showAlertSuccess" 
+    msg="Te has registrado satisfactoriamente. Ya puedes iniciar sesión."
+    @close="handleClose"/>
+    <ErrorAlertComponent v-if="showAlertError"
+    :msg="errorMsg"
+    @close="handleClose"/>
     <div class="h-full flex flex-col align-center ">
         <div class="card w-2/4 items-center m-auto">
             <div class="card-body prose">
                 <h2 class="card-title">Register!</h2>
-                <RegisterFormComponent @form-query="handleFormQuery" />
+                <RegisterFormComponent 
+                @form-query="handleFormQuery" 
+                @show-error="handleShowError"/>
             </div>
         </div>
     </div>
