c6e28df03965aa81f9dca9d67106d4f0e4b093a0
eric <erickmontes555@gmail.com>
2024-04-02 12:25:57 +0200

    Change alert icon

--- src/components/ErrorAlertComponent.vue
+++ src/components/ErrorAlertComponent.vue
@@ -11,7 +11,7 @@ function emitClose() {
     <div role="alert" class="alert alert-error w-2/4 mx-auto my-5">
         <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
         </svg>
         <span>{{ props.msg }}</span>
         <div>
