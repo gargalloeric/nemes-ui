31e2bffe2c1192fef650dfa9db516c54a1d3b697
Alberto Ruiz <albertoruizb14@gmail.com>
2024-04-08 15:15:22 +0200

    Add input-error class when invalid credentials

--- src/components/LoginFormComponent.vue
+++ src/components/LoginFormComponent.vue
@@ -20,7 +20,7 @@ function handleSubmit() {
 
 <template>
     <form @submit.prevent="handleSubmit">
-        <label class="input input-bordered flex items-center gap-2 mb-2">
+        <label class="input input-bordered flex items-center gap-2 mb-2" :class="{'input-error': props.showError}">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                 stroke="currentColor" class="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round"
@@ -29,7 +29,7 @@ function handleSubmit() {
             <input type="text" placeholder="Email" v-model.trim="form.email" class="grow" required />
         </label>
 
-        <label class="input input-bordered flex items-center gap-2 mb-2">
+        <label class="input input-bordered flex items-center gap-2 mb-2" :class="{'input-error': props.showError}">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                 stroke="currentColor" class="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round"
