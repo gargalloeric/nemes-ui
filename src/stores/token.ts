e30e693a7cf873446a7c952529117cf66c158d84
eric <erickmontes555@gmail.com>
2024-04-10 20:28:53 +0200

    Store token when set and search if exists when creating store

--- src/stores/token.ts
+++ src/stores/token.ts
@@ -2,8 +2,16 @@ import { defineStore } from "pinia";
 
 export const useTokenStore = defineStore('token', {
     state: () => {
+        const token = localStorage.getItem('token')
         return {
-            token: null as string | null
+            token: token as string | null
+        }
+    },
+
+    actions: {
+        setToken(token: string) {
+            this.token = token;
+            localStorage.setItem('token', token)
         }
     }
 })
