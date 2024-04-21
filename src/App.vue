c09c83c25431a3db8eaa65ecd5b333261381238c
eric <erickmontes555@gmail.com>
2024-04-21 16:15:56 +0200

    Add navbar to all pages

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
+import NavBar from "./components/NavBar.vue";
 </script>
 
 <template>
+  <NavBar></NavBar>
   <main>
     <RouterView />
   </main>
