79cf4e58e671e69d5ce276f2444d0de5f63babcc
mehdi <mehdigassabarsa@gmail.com>
2024-04-29 16:06:59 +0200

    feat(SP3hu7): cositas

--- src/views/RegisterZoneOfInterest.vue
+++ src/views/RegisterZoneOfInterest.vue
@@ -34,7 +34,36 @@ function save(){
         <div class="text-center mt-4">
           <button class="btn btn-outline">Guardar</button>
         </div>
+
+        <div class="form-control">
+          <h2 class="text-2xl">Lista de catastrofes</h2>
+          <!--<label class="label cursor-pointer">
+            <span class="label-text">Costeros</span>
+            <input type="checkbox" v-model="checkedMeteo" class="checkbox checkbox-info" />
+          </label>
+          <label class="label cursor-pointer">
+            <span class="label-text">Vientos</span>
+            <input type="checkbox" v-model="checkedFire" class="checkbox checkbox-error" />
+          </label>
+          <label class="label cursor-pointer">
+            <span class="label-text">Nevadas</span>
+            <input type="checkbox" v-model="checkedOthers" class="checkbox checkbox-warning" />
+          </label>
+          <label class="label cursor-pointer">
+            <span class="label-text">Lluvias</span>
+            <input type="checkbox" v-model="checkedMeteo" class="checkbox checkbox-info" />
+          </label>
+          <label class="label cursor-pointer">
+            <span class="label-text">Tormentas</span>
+            <input type="checkbox" v-model="checkedFire" class="checkbox checkbox-error" />
+          </label>
+          <label class="label cursor-pointer">
+            <span class="label-text">Nieblas</span>
+            <input type="checkbox" v-model="checkedOthers" class="checkbox checkbox-warning" />
+          </label>!-->
+        </div>
       </form>
+
     </div>
     <div class="flex flex-col mr-4 ml-4">
     </div>
