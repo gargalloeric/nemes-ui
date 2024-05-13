680f3d3d395b8ea783172e666a138cb537d073cf
eric <erickmontes555@gmail.com>
2024-05-13 15:54:03 +0200

    Adjust button size and home icon size

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -16,8 +16,8 @@ const userStore = useUserStore()
                 <ul class="menu menu-horizontal px-1">
                     <li>
                         <RouterLink to="/">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-home" width="28"
-                                height="28" viewBox="0 0 24 24" stroke-width="1" stroke="#2c3e50" fill="none"
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-home" width="24"
+                                height="24" viewBox="0 0 24 24" stroke-width="1" stroke="#2c3e50" fill="none"
                                 stroke-linecap="round" stroke-linejoin="round">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                 <path d="M5 12l-2 0l9 -9l9 9l-2 0" />
